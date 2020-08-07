fc3b0464f7891f321fea35b8d53ded843902a8f7
John Levon <levon@movementarian.org>
2020-08-07 10:51:27 +0000

    bind staging to 0.0.0.0

--- stage.sh
+++ stage.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-ip=$(curl ifconfig.me)
-
 # baseUrl in config file is ignored!
-hugo --config stage.toml --bind $ip --baseUrl http://ssh.movementarian.org:1313/blog/ server
+hugo --config stage.toml --bind 0.0.0.0 --baseUrl http://ssh.movementarian.org:1313/blog/ server
